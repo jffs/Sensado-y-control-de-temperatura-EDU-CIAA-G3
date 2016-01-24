c703414476eef5d860bdaef3851111d73f92af9e
bdellacasa <belen.dellacasa@yahoo.com.ar>
2016-01-24 18:11:34 -0300

    Codigo
    dht22

--- blinking/inc/dht22.h
+++ blinking/inc/dht22.h
@@ -7,5 +7,16 @@
 #define dht22_gpioPort	2
 typedef int8_t byte;
 
+//**************************************************************************
+//* Prototypes
+//**************************************************************************
+
+void dht22_delay_ms (unsigned char time);
+void dht22_delay_us (unsigned char time);
+void dht22_pin_init (void);
+byte leer_datos_dht(void);
+void leer_dht22_pin (float dhthum, float dhttemp);
+float leerTemperatura(void);
+float leerHumedad(void);
 
 #endif /* DHT22_H_ */
--- blinking/src/dht22.c
+++ blinking/src/dht22.c
@@ -1,5 +1,7 @@
 #include "chip.h"
 #include "dht22.h"
+#include "ciaaPOSIX_stdio.h"  /* <= device handler header */
+#include "ciaaPOSIX_string.h" /* <= string header */
 
 
 byte dht22_pin_dat[5];
@@ -30,10 +32,10 @@ void dht22_delay_us (unsigned char time)
 void dht22_pin_init (void){
    Chip_SCU_PinMux(dht22_port,dht22_pin,SCU_MODE_INACT | SCU_MODE_INBUFF_EN | SCU_MODE_ZIF_DIS,FUNC0);
    Chip_GPIO_SetDir(LPC_GPIO_PORT,dht22_gpioPort,(1<<dht22_gpioPin),!1);//dth22 como input
-   //dht_io=0;
+   
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,0);//dth22 -> 0
    dht22_delay_ms(1);
-   //output_high(dht22_pin);
+   
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,1);//dth22 -> 1
 }
 
@@ -60,26 +62,25 @@ void leer_dht22_pin (float dhthum, float dhttemp){
    byte dht22_pin_in, i, dht22_pin_checksum;
   
    
-   //dht_io=0; // configurar el pin como salida
+	// configurar el pin como salida
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,0);//dth22 -> 0
-   //output_high(dht22_pin);
+  
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,1);//dth22 -> 1
    dht22_delay_us(20);
-   //output_low(dht22_pin);
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,0);//dth22 -> 0
    dht22_delay_ms(18);
    
-   //output_high(dht22_pin);
+  
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,1);//dth22 -> 1
    dht22_delay_us(22);
-   //dht_io=1;// configurar el pin como entrada
+   // configurar el pin como entrada
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,1);//dth22 -> 0
    dht22_delay_us(5);
    dht22_pin_in=Chip_GPIO_GetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin);
    
    if(dht22_pin_in) {
       //GlobalErr=1;
-      printf("\r\ndht condicion 1 de inicio no encontrada");
+      ciaaPOSIX_printf("\r\nDHT checksum error");
       return;
    }
    
@@ -88,7 +89,7 @@ void leer_dht22_pin (float dhthum, float dhttemp){
    
    if(!dht22_pin_in){
       //GlobalErr=2;
-      printf("\r\ndht condicion 2 de inicio no encontrada");
+      ciaaPOSIX_printf("\r\nDHT checksum error");
       return;
    }
    
@@ -98,17 +99,17 @@ void leer_dht22_pin (float dhthum, float dhttemp){
       dht22_pin_dat[ i ] = leer_datos_dht(); // capturando datos
    }
    
-   //dht_io=0;
+  
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,0);//dth22 -> 0
    dht22_delay_us(10);
-   //output_high(dht22_pin);
+   
    Chip_GPIO_SetPinState(LPC_GPIO_PORT,dht22_gpioPort,dht22_gpioPin,1);//dth22 -> 1
    
    dht22_pin_checksum= dht22_pin_dat[0]+dht22_pin_dat[1]+dht22_pin_dat[2]+dht22_pin_dat[3];
    
    if(dht22_pin_dat[4]!=dht22_pin_checksum){
       //GlobalErr=3;
-      printf("\r\nDHT checksum error");
+      ciaaPOSIX_printf("\r\nDHT checksum error");
    }
    
    dhthum  = (float)dht22_pin_dat[0] * 256 + (float)dht22_pin_dat[1];
